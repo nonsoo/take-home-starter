6a5bde51bf83b0bb7f972e54bb0bd8b45f995553
Nonso <nonsootoh@gmail.com>
2022-09-03 07:18:15 -0400

    feat: adds review fields names

--- react-ts-starter/src/components/ReviewForm.tsx
+++ react-ts-starter/src/components/ReviewForm.tsx
@@ -7,6 +7,23 @@ const ReviewForm: FC = () => {
         Love to hear from you, <br />
         Get in touch!
       </p>
+
+      <div className="reviewItem">
+        <p className="reviewItem__Label">First Name</p>
+        <p className="reviewItem__item">John Smith</p>
+      </div>
+      <div className="reviewItem">
+        <p className="reviewItem__Label">Last Name</p>
+        <p className="reviewItem__item">Robinson</p>
+      </div>
+      <div className="reviewItem">
+        <p className="reviewItem__Label">Phone number</p>
+        <p className="reviewItem__item">6479952269</p>
+      </div>
+      <div className="reviewItem">
+        <p className="reviewItem__Label">Email Address</p>
+        <p className="reviewItem__item">jR@gmail.com</p>
+      </div>
     </section>
   );
 };
